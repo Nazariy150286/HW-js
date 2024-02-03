7692ffe0967ee7943248bd10fbe534961fe63599
Nazariy <nazarij1608@gmail.com>
2024-02-03 19:22:28 +0200

    changes html, added js

---
+++ hw-1/index.js
@@ -0,0 +1,52 @@
+//! Task 1
+// let age = 16;
+// console.log(age);
+
+//! Task 2
+// const name = "Nazariy";
+// console.log(name);
+
+//! Task 3
+// const isStudent = true;
+// console.log(isStudent);
+
+//! Task 4
+// const myString = "В Ідеї Nація міцна!";
+// console.log(myString);
+
+//! Task 5
+//version 2
+// let myNumber = 7 + 10;
+
+// console.log(myNumber);
+
+// version 2
+// let myNumber = 7;
+// const plusToMyNumber = 10;
+// const result = myNumber + plusToMyNumber;
+
+// console.log(result);
+
+//! Task 6
+// const myNull = null;
+
+// console.log(myNull);
+
+//! Task 7
+// const yourName = prompt("Будь ласка, напишіть ваше ім'я");
+// const welcome = alert(yourName + ", вітаємо вас на нашому сайті!");
+
+// console.log(nameWelcome);
+
+//! Task 8
+// const phoneNumber = prompt("Підтвердіть свій номер телефону!");
+// const whenUserClickOk = "Дякую за підтвердження!";
+// const whenUserClickCancel = "Дію відмінено!";
+// alert(whenUserClickOk);
+// alert(whenUserClickCancel);
+//! Task 9
+// const phoneNumber = confirm("Ця дія є небезпечною!");
+// const whenUserClickOk = "Дякую за підтвердження!";
+// const whenUserClickCancel = "Дію відмінено!";
+// alert(whenUserClickOk);
+// alert(whenUserClickCancel);
