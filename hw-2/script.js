812c738291515e72e9123484cddc75d0c99a1157
Nazariy <nazarij1608@gmail.com>
2024-02-08 22:06:27 +0200

    changes html,js

--- hw-2/script.js
+++ hw-2/script.js
@@ -9,6 +9,54 @@
 // console.log((temperatureFahrenheit - 26) / 2);
 
 //! Task 2
-const daysInTheMonth = 30.4368499;
-console.log(daysInTheMonth * 24); // Скільки годин в місяці? / 744
-console.log(daysInTheMonth * 24 * 60); // Скільки хвилин в одному місяці? / 44640
+// const daysInTheMonth = 31;
+// console.log(daysInTheMonth * 24); // Скільки годин в місяці? / 744
+// console.log(daysInTheMonth * 24 * 60); // Скільки хвилин в одному місяці? / 44640
+
+//! Інформація є з відносною похибкою, так як в середньому днів в місяці 30.3
+
+//! Task 3
+
+// const playerHealth = 100;
+// const playerSpeed = 100;
+
+// const playerResultHealth = playerHealth - 37;
+// const playerResultSpeed = playerSpeed - 78;
+
+// console.log(`Health:`, playerResultHealth);
+// console.log(`Speed:`, playerResultSpeed);
+
+//! Task 4
+// const shopSum = 867;
+// const shopDiscount = 10;
+// const discountResult = 867 / 10;
+
+// console.log(discountResult);
+
+//! Task 5
+// const num = 3.14;
+// console.log(Math.floor(num));
+
+//! Task 6
+// const num = 3.14;
+// console.log(parseFloat(num));
+
+//! Task 7
+// const imposter = "3.1453476356983750837689760";
+
+// console.log(parseInt(imposter));
+
+//! Task 8
+// const num = 7;
+// console.log(Math.sqrt(num));
+
+//! Task 9
+// const num = 9;
+// const line = "9";
+// parseInt(line);
+// toString(num);
+// console.log(num, line);
+
+// const sum = num + line;
+
+// console.log(sum);
