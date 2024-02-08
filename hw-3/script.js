0902e61232170c00688c100504ba23d9eb3fbf11
Nazariy <nazarij1608@gmail.com>
2024-02-08 22:46:17 +0200

    changes html,js

---
+++ hw-3/script.js
@@ -0,0 +1,38 @@
+//! Task 1
+
+// const result = 5 + 5 + "5";
+
+// console.log(typeof result);
+
+//! Task 2
+
+// const email = "nazarij1608@gmail.com";
+// if (email.includes("@")) {
+//   console.log("Рядок містить символ @ ");
+// } else {
+//   console.log("Рядок не містить символ @ ");
+// }
+// console.log(email.length);
+// console.log(email);
+
+//! Task 3
+// const word1 = "My ";
+// const word2 = "name ";
+// const word3 = "is ";
+
+// const resultWord = word1 + word2 + word3;
+
+// const myName = "Victor";
+
+// const result = resultWord + myName;
+
+// console.log(result);
+
+//! Task 4
+
+const userName = "Nazariy";
+const payment = "768";
+const result =
+  "Дякуємо " + userName + "! " + "До " + "сплати " + payment + " гривень";
+
+alert(result);
