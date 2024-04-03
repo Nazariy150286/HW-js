071b49e417a6a4e0a0ca8a9b70d1c9ae389c9726
Nazariy <nazarij1608@gmail.com>
2024-04-03 11:14:53 +0300

    changes html,css

---
+++ hw7 - Масиви/js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+//! Task 1
+// Створити масив з трьох чисел. Змінити значення другого елемента масиву на 10.
+// const massive = [5,12,20]
+// massive [1] = 10
+// console.log(massive);
+
+//! Task 2
+//Створити масив із трьох рядків. Додати до масиву ще одну рядків.
+
+// const massive = ['1 Рядок', '2 Рядок', '3 Рядок']
+// const newString = "4 Рядок"
+
+// massive.push(newString)
+// console.log(massive);
+
+//! Task 3
+//Створити скрипт який поверне суму всіх чисел в масиві.
+// let massive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// let total = 0;
+
+// for (let num of massive) {
+//   total += num;
+// }
+
+// console.log("Сума чисел у масиві: ", total);
+
+//! Task 4
+//Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
+// const massive = [1, 4, 8, 8, 0];
+// for (let num of massive) {
+//   console.log(num);
+// }
+
+//! Task 5
+//Створити масив із 5-ти рядків. Вивести на екран кожен рядокз масиву, який містить більше 5-ти символів.
+// const massive = ["Bye!", "1234", "1234567890", "Hello World!", "Hello"];
+
+// for (let str of massive) {
+//   if (str.length > 5) {
+//     console.log(str);
+//   }
+// }
+
+//! Task 6
+// Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
+
+// const massive = [34, 77, 98, 32, 12, 45, 67, 43, 76, 55];
+// let maxNumber = massive[0];
+
+// for (let i = 1; i < massive.length; i++) {
+//   if (massive[i] > maxNumber) {
+//     maxNumber = massive[i];
+//   }
+// }
+// console.log("Максимальне значення в масиві: ", maxNumber);
+
+//! Task 7
+//Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
+// const massive = [2, 13, 24, 36, 47, 58, 69, 74, 87, 98];
+// for (let i = 0; i < massive.length; i++) {
+//   if (massive[i] % 2 === 0) {
+//     console.log(massive[i] + " - парне число");
+//   }
+// }
